b272893e6f3c143180dfb94f80a7b3047e217361
Dan Swain <dan.t.swain@gmail.com>
2018-01-12 18:22:35 -0500

    Add a basic layout

--- hm_web/tests/test_main_page.py
+++ hm_web/tests/test_main_page.py
@@ -1,9 +1,25 @@
+"""
+Tests for index.html
+"""
+
 import pytest
 
 
 assert pytest
 
 
+def template_used(resp, template_name):
+    """
+    Returns True if the given template was used
+    """
+    for template in resp.templates:
+        if template.name == template_name:
+            return True
+
+    return False
+
+
 def test_simple_page(client):
     resp = client.get('/')
     assert resp.status_code == 200
+    assert template_used(resp, 'hm_web/index.html')
--- hm_web/views.py
+++ hm_web/views.py
@@ -1,6 +1,12 @@
+'''
+Heavy Meta main views module
+'''
+
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    '''
+    Index view
+    '''
+    return render(request, 'hm_web/index.html', {})
