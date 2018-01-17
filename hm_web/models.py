670888e214b6a7e571fc4c8ec42ea47367e2c45e
Dan Swain <dan.t.swain@gmail.com>
2018-01-16 22:17:07 -0500

    Initial model definitions

--- hm_web/models.py
+++ hm_web/models.py
@@ -1,3 +1,54 @@
-from django.db import models
+"""
+Model definitions
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Song(models.Model):
+    """
+    Represents a song - a collection of tracks
+    """
+    title = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    note = models.TextField()
+
+
+class Track(models.Model):
+    """
+    Represents a recording track
+    """
+    title = models.CharField(max_length=200)
+    song_order = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    note = models.TextField()
+
+
+class Take(models.Model):
+    """
+    Represents a recording track
+    """
+    title = models.CharField(max_length=200)
+    track_order = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    note = models.TextField()
+
+
+class TakeComment(models.Model):
+    """
+    Represents user discussion around a take
+    """
+    take = models.ForeignKey(Take, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    body = models.TextField()
